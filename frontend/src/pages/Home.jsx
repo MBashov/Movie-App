3a647d6704d10f5c38bee4a7d0ec6a4da486d3fc
MBashov <galacticos.mb@gmail.com>
2025-05-04 19:16:01 +0200

    Add search field

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react'
+
 import MovieCard from "../components/MovieCard";
 
 export default function Home() {
+
+    const [searchQuery, setSearchQuery] = useState('');
 
     const movies = [
         { id: 1, title: 'Terminator', release_date: 1998 },
         { id: 2, title: 'John Wick', release_date: 2020 },
         { id: 3, title: 'Matrix', release_date: 2000 },
-    ]
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchQuery);
+
+    }
 
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                    type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-btn">Search</button>
+            </form>
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
