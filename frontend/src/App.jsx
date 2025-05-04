eb14c81cfc13251966fa1496f3f965554b4d6627
MBashov <galacticos.mb@gmail.com>
2025-05-04 18:52:55 +0200

    Add home page with hardcoded movies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Home from './pages/home'
 
 function App() {
 
-    const movies = [
-        {id: 1, title: 'Terminator', 'release-date': 2020}
-    ]
-
     return (
         <div>
-            <MovieCard movie={{id: 1, title: 'Terminator', 'release_date': 2020}}/>
-            <MovieCard movie={{id: 2, title: 'Pirates', 'release_date': 2022}}/>
+            <Home />
         </div>
     )
 }
