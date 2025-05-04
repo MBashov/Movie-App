3c709129cc1208db3a1ba5f4ed06e79be8fb6416
MBashov <galacticos.mb@gmail.com>
2025-05-04 19:40:00 +0200

    Add navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import './App.css'
 
 import Home from './pages/home'
 import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 
 function App() {
 
     return (
-        <main className='main-content'>
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/favorites' element={<Favorites />} />
-            </Routes>
-        </main>
+        <div>
+            <NavBar />
+            <main className='main-content'>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/favorites' element={<Favorites />} />
+                </Routes>
+            </main>
+        </div>
     )
 }
 
